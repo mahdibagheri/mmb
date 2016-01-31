2b459a817d2befbd9a4b38a855ebbee68b50e550
mahdibagheri <150mahdi@gmail.com>
2016-01-31 10:15:58 -0800

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {77750573},
+    sudo_users = {77750573,103998262},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
