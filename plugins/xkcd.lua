4b5990e284f90ae26eae1a632e19eea2b344ee45
mahdibagheri <150mahdi@gmail.com>
2016-01-31 10:09:39 -0800

    Update xkcd.lua

--- plugins/xkcd.lua
+++ plugins/xkcd.lua
@@ -1,58 +1,42 @@
-do
-
-function get_last_id()
-  local res,code  = https.request("http://xkcd.com/info.0.json")
-  if code ~= 200 then return "HTTP ERROR" end
-  local data = json:decode(res)
-  return data.num
-end
-
-function get_xkcd(id)
-  local res,code  = http.request("http://xkcd.com/"..id.."/info.0.json")
-  if code ~= 200 then return "HTTP ERROR" end
-  local data = json:decode(res)
-  local link_image = data.img
-  if link_image:sub(0,2) == '//' then
-    link_image = msg.text:sub(3,-1)
-  end
-  return link_image, data.title, data.alt
-end
-
-
-function get_xkcd_random()
-  local last = get_last_id()
-  local i = math.random(1, last)
-  return get_xkcd(i)
-end
-
-function send_title(cb_extra, success, result)
-  if success then
-    local message = cb_extra[2] .. "\n" .. cb_extra[3]
-    send_msg(cb_extra[1], message, ok_cb, false)
-  end
-end
-
-function run(msg, matches)
+local function set_bot_photo(msg, success, result)
   local receiver = get_receiver(msg)
-  if matches[1] == "!xkcd" then
-    url, title, alt = get_xkcd_random()
+  if success then
+    local file = 'data/photos/bot.jpg'
+    print('File downloaded to:', result)
+    os.rename(result, file)
+    print('File moved to:', file)
+    set_profile_photo(file, ok_cb, false)
+    send_large_msg(receiver, 'Photo changed!', ok_cb, false)
+    redis:del("bot:photo")
   else
-    url, title, alt = get_xkcd(matches[1])
+    print('Error downloading: '..msg.id)
+    send_large_msg(receiver, 'Failed, please try again!', ok_cb, false)
   end
-  file_path = download_to_file(url)
-  send_photo(receiver, file_path, send_title, {receiver, title, alt})
-  return false
 end
 
+local function run(msg,matches)
+    local data = load_data(_config.moderation.data)
+    local receiver = get_receiver(msg)
+    local group = msg.to.id
+    if not is_admin(msg) then
+      return
+    end
+    if msg.media then
+        if msg.media.type == 'photo' and redis:get("bot:photo") then
+          if redis:get("bot:photo") == 'waiting' then
+            load_photo(msg.id, set_bot_photo, msg)
+          end
+        end
+    end
+    if matches[1] == "setbotphoto" then
+      redis:set("bot:photo", "waiting")
+      return 'Please send me bot photo now'
+    end
+end
 return {
-  description = "Send comic images from xkcd",
-  usage = {"!xkcd (id): Send an xkcd image and title. If not id, send a random one"},
   patterns = {
-    "^!xkcd$",
-    "^!xkcd (%d+)",
-    "xkcd.com/(%d+)"
+  "^[!/](setbotphoto)$",
+  "%[(photo)%]"
   },
-  run = run
+  run = run,
 }
-
-end
